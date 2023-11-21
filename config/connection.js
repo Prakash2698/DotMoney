279baa553ee6be4026e173f86e5cb3c3a478f1fe
Prakash2698 <prakashv2698@gmail.com>
2023-11-21 13:12:15 +0530

    final

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
-// const url = "mongodb+srv://webersedigialifeapi:webersedigialifeapi@digialifeapipanel.5sgkx4e.mongodb.net/dotMoney";
+const url = "mongodb+srv://webersedigialifeapi:webersedigialifeapi@digialifeapipanel.5sgkx4e.mongodb.net/dotMoney";
 
-const url = "mongodb://0.0.0.0:27017/DOTMoney"
+// const url = "mongodb://0.0.0.0:27017/DOTMoney"
 const mongodb= mongoose.connect(url,{
     // useNewUrlParser: true,
     // useUnifiedTopology: true,
